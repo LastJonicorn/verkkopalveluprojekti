d77184912806eed4e6d47a96bb1bf6f5379e7e54
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2022-03-22 19:44:56 +0200

    Pikkusen hain asettelua jo sivulle

--- src/App.js
+++ src/App.js
@@ -1,48 +1,91 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Carousel from 'react-bootstrap/Carousel';
+import Navbar from 'react-bootstrap/Navbar';
+import {Container, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
 
 function App() {
+  
   return (
     <div className="App">
+      <h1>Lautapelikahvila Weleho</h1>
+<Navbar bg="dark" expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#">LOGO</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Etusivu</Nav.Link>
+        <Nav.Link href="#action2">FAQ</Nav.Link>
+        <NavDropdown title="Kategoriat" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Roolipelit</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Korttipelit</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">
+            Deck Building
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#">
+          Link
+        </Nav.Link>
+      </Nav>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Hae"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Hae</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
       <div class="row">
     <div class="col-8"><Carousel variant="dark">
   <Carousel.Item>
     <img
-      className="d-block w-50"
-      src="..//Kuvat/IMG_20180719_214837.jpg"
+      className="d-block w-10"
+      src="../Kuvat\1782_1200x1200.jpg"
       alt="First slide"
     />
     <Carousel.Caption>
-      <h5>First slide label</h5>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <h5>Tässä peli nro 1</h5>
+      <p>Vau mikä peli. Haluaisit varmasti ostaa tämän!</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-50"
-      src="../Kuvat/IMG_20180719_214826.jpg"
+      className="d-block w-20"
+      src="../Kuvat\pic332870.jpg"
       alt="Second slide"
     />
     <Carousel.Caption>
-      <h5>Second slide label</h5>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h5>Tässä peli nro 2</h5>
+      <p>Vau mikä peli. Haluaisit varmasti ostaa tämän!</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-50"
-      src="../Kuvat/IMG_20180719_214850.jpg"
+      className="d-block w-30"
+      src="../Kuvat\pic3518231.jpg"
       alt="Third slide"
     />
     <Carousel.Caption>
-      <h5>Third slide label</h5>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h5>Tässä peli nro 3</h5>
+      <p>Vau mikä peli. Haluaisit varmasti ostaa tämän!</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel></div>
-    <div class="col-4">djfghfgkjgjlfdljfsjlfsjlfsljjlsljsjlljjlgsrgjl</div>
+    <div class="col-4">Tähän alkaa syntyä meidän blogikirjoituksemme.
+    <div class="suosittelut"></div>
+    </div>
   </div>
+  <footer>THIS IS FOOTER</footer>
   </div>
   );
 }
