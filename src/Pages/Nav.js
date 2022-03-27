da7fe76154bd1f911ae6d25c4e0f7f22294e6e0d
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2022-03-27 17:43:21 +0300

    navbar

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -7,7 +7,7 @@ function App() {
 
         
   return (
-<Navbar bg="white" expand="lg" className='Navbar'>
+<Navbar bg="white" expand="lg" id='navbar'>
   <Container fluid>
     <Navbar.Brand href="#">
       <img 
@@ -22,8 +22,8 @@ function App() {
         style={{ maxHeight: '100px'}}
         navbarScroll
       >
-        <Nav.Link href="#action1">Etusivu</Nav.Link>
-        <Nav.Link href="#action2">FAQ</Nav.Link>
+        <Nav.Link id="link" href="#action1">Etusivu</Nav.Link>
+        <Nav.Link id="link" href="#action2">FAQ</Nav.Link>
         <NavDropdown title="Kategoriat" id="navbarScrollingDropdown">
           <NavDropdown.Item href="#action3">Roolipelit</NavDropdown.Item>
           <NavDropdown.Item href="#action4">Korttipelit</NavDropdown.Item>
@@ -32,7 +32,7 @@ function App() {
             Deck Building
           </NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link href="#">
+        <Nav.Link href="#" id="link">
           Link
         </Nav.Link>
       </Nav>
