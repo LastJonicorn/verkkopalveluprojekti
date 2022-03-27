0fdf995c240d2912010690797dc15662521c469d
Joni Lampela <93704834+LastJonicorn@users.noreply.github.com>
2022-03-27 14:42:45 +0300

    En saanu navbaria toimimaan. Vaihdoin Bootstrapiin

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -1,23 +1,41 @@
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Link } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
+import Navbar from 'react-bootstrap/Navbar';
+import {Container, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
 
 function App() {
 
         
   return (
-    <nav className='nav'>
-      <h3>LOGO</h3>
-      <ul className='nav-links'>
-        <Link to="/">
-          <li>Etusivu</li>
-        </Link>
-        <Link to="./Pages/kysymykset">
-          <li>FAQ</li>
-        </Link>
-        <li>Kategoriat</li>
-      </ul>
-    </nav>
+<Navbar bg="white" expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#">LOGO</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Etusivu</Nav.Link>
+        <Nav.Link href="#action2">FAQ</Nav.Link>
+        <NavDropdown title="Kategoriat" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Roolipelit</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Korttipelit</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">
+            Deck Building
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#">
+          Link
+        </Nav.Link>
+      </Nav>
+
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     
   );
 }
