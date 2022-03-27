b8cc55f179e17dc5839f0081c3ca14e93964ce2b
CaritaNi <93704532+CaritaNi@users.noreply.github.com>
2022-03-27 18:04:10 +0300

    Columnien säätöä

--- src/Pages/Etusivu.js
+++ src/Pages/Etusivu.js
@@ -78,12 +78,14 @@ function App() {
   <div className="container p-6">
     <div className="row">
       <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 className="text">Footer text</h5>
+        <h5 className="text">Yritystiedot</h5>
 
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-          aliquam voluptatem veniam, est atque cumque eum delectus sint!
+          p. 05050505050 <br></br>
+          Tämä osoite on legit <br></br>
+          xxxxxx OULU <br></br>
+          asiakaspalvelu@weleho.com
+          
         </p>
       </div>
 
