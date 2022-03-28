ec872bf1398dc9e7b0d2219ef616507ec4042959
LastJonicorn <joni.k.lampela@gmail.com>
2022-03-28 13:23:57 +0300

    Navbar tyylit hävisi, mutta linkit toimii

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -2,42 +2,43 @@ import '../App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from 'react-bootstrap/Navbar';
 import {Container, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 function App() {
-
-        
+  
   return (
-<Navbar bg="white" expand="lg" id='navbar'>
-  <Container fluid>
-    <Navbar.Brand href="#">
-      <img 
-      src='../Kuvat/logo.png'
-      className='logo'
-      />
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="me-auto my-2 my-lg0"
-        style={{ maxHeight: '100px'}}
-        navbarScroll
-      >
-        <Nav.Link id="link" href="#action1">Etusivu</Nav.Link>
-        <Nav.Link id="link" href="#action2">FAQ</Nav.Link>
-        <NavDropdown title="Kategoriat" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Lautapelit</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Roolipelit</NavDropdown.Item>
-          <NavDropdown.Item href="#action5">Korttipelit</NavDropdown.Item>
-          <NavDropdown.Item href="#action6">Deck Building</NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#" id="link">
-          Link
-        </Nav.Link>
-      </Nav>
-
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/kysymykset">FAQ</Link>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     
   );
 }
