d629ef574d19ad6d7283ff78fd7855e3ec948c9b
LastJonicorn <joni.k.lampela@gmail.com>
2022-03-28 13:41:18 +0300

    Navbar samanlainen kuin ennen

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -9,14 +9,14 @@ function App() {
   return (
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#"><img src='../Kuvat/logo.png'/></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <Link class="nav-link active" id="link" aria-current="page" to="/">Home</Link>
+          <Link class="nav-link active" id="link" aria-current="page" to="/">Etusivu</Link>
         </li>
         <li class="nav-item">
           <Link class="nav-link" id="link" to="/kysymykset">FAQ</Link>
