88d18a45d2ceafea4ea29326f9796183abfc97be
LastJonicorn <joni.k.lampela@gmail.com>
2022-03-29 11:21:12 +0300

    Annoin FAQ sivulle paddingia

--- src/Pages/kysymykset.js
+++ src/Pages/kysymykset.js
@@ -13,27 +13,26 @@ function App() {
       <div class="container">
       <div class="row">
         <div class="col-6">      
-          <h3>Voinko toivoa tiettyä peliä valikoimiinne?</h3>
-          <p>
+          <h3 className='kysymys'>Voinko toivoa tiettyä peliä valikoimiinne?</h3>
+          <p className='vastaus'>
             Kyllä. Voit lähettää meille sähköpostia otsikolla "pelitoive" ja toivoa haluamaasi peliä valikoimiimme. <br/>
             Jos saamme tarpeeksi toiveita tietyistä peleistä niin lisäämme ne varmasti valikoimaan tulevaisuudessa.
           </p>
 
-          <h3>Tuotteeni on aina loppu? Miksi?</h3>
-          <p>
+          <h3 className='kysymys'>Tuotteeni on aina loppu? Miksi?</h3>
+          <p className='vastaus'>
             Varastomme ovat ikävä kyllä rajalliset ja osa tuotteistamme on selvästi toivotumpia kuin toiset.
             Halutessasi voit jättää tuotteen kohdalle sähköpostisi ja ilmoitamme sinulle, kun tuote on taas tilattavissa.
           </p>
       </div>
         <div class="col-6">     
-        <h3>Voinko toivoa tiettyä peliä valikoimiinne?</h3>
-          <p>
-            Kyllä. Voit lähettää meille sähköpostia otsikolla "pelitoive" ja toivoa haluamaasi peliä valikoimiimme. <br/>
-            Jos saamme tarpeeksi toiveita tietyistä peleistä niin lisäämme ne varmasti valikoimaan tulevaisuudessa.
+        <h3 className='kysymys'>Onko kaikki mitä ihminen sanoo ennen "mutta" sanaa valhetta?</h3>
+          <p className='vastaus'>
+            Kyllä.
           </p>
 
-          <h3>Tuotteeni on aina loppu? Miksi?</h3>
-          <p>
+          <h3 className='kysymys'>Tuotteeni on aina loppu? Miksi?</h3>
+          <p className='vastaus'>
             Varastomme ovat ikävä kyllä rajalliset ja osa tuotteistamme on selvästi toivotumpia kuin toiset.
             Halutessasi voit jättää tuotteen kohdalle sähköpostisi ja ilmoitamme sinulle, kun tuote on taas tilattavissa.
           </p>
