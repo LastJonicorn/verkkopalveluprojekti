848402a6c25c546c38e82567a3464896b95f1efc
LastJonicorn <joni.k.lampela@gmail.com>
2022-03-30 11:01:18 +0300

    Ostoskoriin lisätty ihan basic juttuja vaan

--- src/Pages/Ostoskori.js
+++ src/Pages/Ostoskori.js
@@ -15,6 +15,15 @@ function App() {
             <div class="col-8">
                 <h2>Ostoskori</h2>
                 <p>Tuotteesi</p>
+                <div id='OstoskoriTuote'>
+                    <img src='../Kuvat/pic332870.jpg'/>
+                    <p>Hinta: 45€</p>
+                    <p>Tuotenumero: 123456789</p>
+
+                    <img src='../Kuvat/pic332870.jpg'/>
+                    <p>Hinta: 45€</p>
+                    <p>Tuotenumero: 123456789</p>
+                </div>
                 <p>Maksutavat</p>
             </div>
             <div class="col-3">
