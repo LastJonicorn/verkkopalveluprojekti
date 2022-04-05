b306ccde11ec34b033fdca5cb2828a85517b0a0c
Liisa Pikkarainen <liisapikkarainen1@gmail.com>
2022-04-05 15:34:49 +0300

    L.pelit koodin pohja kopsattu,mutta teksti harmaa?

--- src/Pages/Lautapelit.js
+++ src/Pages/Lautapelit.js
@@ -1,66 +1,21 @@
 import React from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Carousel from 'react-bootstrap/Carousel';
 
 const URL = '';
 
 function App() {
   
   return (
-    <div className="App">
+    <div className="App"></div>
+  )
 
 {/*Navbar alkaa tästä*/}
 
 {/*Grid Layout alkaa tästä*/}
 
-      <div className="row">
+    <div className="row">
     <div className="col-7">
-
-{/*Carousel alkaa tästä*/}
-
-  <Carousel variant="dark">
-  <Carousel.Item>
-    <img
-      className="d-block w-10"
-      src="../Kuvat\carcassonne-the-princess-the-dragon-nordic-2925iA9D2C7A24C5CBDD72EA339CC093B9330A8EBE9A7.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h5 class="pelinimi">Tässä peli nro 1</h5>
-      <p class="kuvaus">Vau mikä peli. Haluaisit varmasti ostaa tämän!</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-20"
-      src="../Kuvat\pic332870.jpg"
-      alt="Second slide"
-    />
-    <Carousel.Caption>
-      <div className="peliteksti">
-        <h5 className="pelinimi">Tässä peli nro 2</h5>
-        <p className="kuvaus">Vau mikä peli. Haluaisit varmasti ostaa tämän!</p>
-      </div>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-30"
-      src="../Kuvat\pic3518231.jpg"
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h5 className="pelinimi">Tässä peli nro 3</h5>
-      <p className="kuvaus">Vau mikä peli. Haluaisit varmasti ostaa tämän!</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-<br></br>
-<p>
-  Tuleeko tähän vielä jokin oma juttunsa? Kuukauden suosituimmat tuotteet? Alennukset?
-</p>
-</div>
 
 {/*Grid Layout toinen puoli alkaa tästä*/}
 
@@ -98,7 +53,7 @@ function App() {
       />
       </div>
       <div class="col-lg-5 col-sm-4 right" id="oikea">
-               <h5 className="oikea">Löydät meidät myös somesta!</h5>
+        <h5 className="oikea">Löydät meidät myös somesta!</h5>
 
         <p>
         <a href="#" class="fa fa-facebook"></a>
@@ -111,7 +66,8 @@ function App() {
   </div>
   </footer>
   </div>
-  );
+  ;
+  
 }
 
 export default App;
