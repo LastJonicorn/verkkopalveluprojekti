2ccada495d37389960df7678c3a9df3d0fa77bfe
Johankivilomplo <n0kijo00@students.oamk.fi>
2022-04-05 15:37:39 +0300

    Palaute.js luotu

---
+++ src/Pages/Palaute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from 'react-bootstrap/Navbar';
+import {Container, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+
+function App() {
+    return
+
+    
+};
