dc3a5540ba156fafc9471c8880587ea7f87f81ec
CaritaNi <93704532+CaritaNi@users.noreply.github.com>
2022-04-05 14:29:32 +0300

    FAQ tyylittely

--- src/Pages/kysymykset.js
+++ src/Pages/kysymykset.js
@@ -14,7 +14,7 @@ function App() {
       <Nav />
       <div class="container">
       <Accordion defaultActiveKey={['0']} alwaysOpen>
-  <Accordion.Item eventKey="0">
+  <Accordion.Item eventKey="1">
     <Accordion.Header>Voinko toivoa tiettyä peliä valikoimiinne?</Accordion.Header>
     <Accordion.Body>
     Kyllä. Voit lähettää meille sähköpostia otsikolla "pelitoive" ja toivoa haluamaasi peliä valikoimiimme. <br/>
@@ -22,35 +22,35 @@ function App() {
           
     </Accordion.Body>
   </Accordion.Item>
-  <Accordion.Item eventKey="1">
+  <Accordion.Item eventKey="2">
     <Accordion.Header>Tuotteeni on aina loppu? Miksi?</Accordion.Header>
     <Accordion.Body>
     Varastomme ovat ikävä kyllä rajalliset ja osa tuotteistamme on selvästi toivotumpia kuin toiset.
             Halutessasi voit jättää tuotteen kohdalle sähköpostisi ja ilmoitamme sinulle, kun tuote on taas tilattavissa.
     </Accordion.Body>
   </Accordion.Item>
-  <Accordion.Item eventKey="2">
+  <Accordion.Item eventKey="3">
     <Accordion.Header>Onko tuotteillanne palautusoikeutta?</Accordion.Header>
     <Accordion.Body>
     Kaikilla AVAAMATTOMILLA tuotteillamme on 30 päivän palautusoikeus.
     </Accordion.Body>
-    /</Accordion.Item>
-    <Accordion.Item eventKey="3">
+    </Accordion.Item>
+    <Accordion.Item eventKey="4">
     <Accordion.Header>Onko kaikki mitä ihminen sanoo ennen "mutta" sanaa valhetta?</Accordion.Header>
     <Accordion.Body>
     Kyllä.
     </Accordion.Body>
-    /</Accordion.Item>
-    <Accordion.Item eventKey="4">
+    </Accordion.Item>
+    <Accordion.Item eventKey="5">
     <Accordion.Header>Sain viallisen tuotteen. Mitä teen?</Accordion.Header>
     <Accordion.Body>
     Ole meihin sähköpostilla yhteydessä. Viallisen tuotteen hyvitämme lähettämällä uuden vastaavan tilalle mahdollisimman pian.
             Pahoittelemme viallisen tuotteen saapumisesta
     </Accordion.Body>
-    /</Accordion.Item>
+    </Accordion.Item>
 
 </Accordion>
-      /</div>
+      </div>
     </div>
   );
 }
