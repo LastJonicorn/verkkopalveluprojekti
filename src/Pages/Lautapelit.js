b7f71690b9c31d1716742ae587725a16d7fa5c8c
liisapikkarainen <liisapikkarainen1@gmail.com>
2022-04-06 11:25:23 +0300

    Card linkitys kesken

--- src/Pages/Lautapelit.js
+++ src/Pages/Lautapelit.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../Lautapelit.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Card from 'react-bootstrap/Card';
+
 
 const URL = '';
 
@@ -12,25 +14,47 @@ function App() {
       <div className="row">
       <div className="col-7">
 
-      <div className="col" id="ale">
-    <ul class="nav flex-column" id="ale" > 
-          <li class="nav-item" id="aletuote">
-            <a class="nav-link" href="#">aletuote</a>
-            <img src="..\Kuvat\i25369691w800h465.jpg" id="alekuva"/>
-          </li>
-          <li class="nav-item"  id="aletuote">
-            <a class="nav-link" href="#">aletuote</a>
-            <img src="../Kuvat/1782_1200x1200.jpg" id="alekuva"/>
-          </li>
-          <li class="nav-item"  id="aletuote">
-            <a class="nav-link" href="#">aletuote</a>
-            <img src="../Kuvat/4015566033481xxl.jpg" id="alekuva"/>
-          </li>
-    </ul>
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
     </div>
-
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
     </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
     </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="..."></img>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+      
+    </div>
+  </div>  
   </div>
   );
   
