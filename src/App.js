a3dedf729523e24ff21fa6359a7e5e77c89aa551
CaritaNi <carita.niskanen@hotmail.com>
2022-04-11 13:22:25 +0300

    ostoskorin app.js koodi

--- src/App.js
+++ src/App.js
@@ -9,10 +9,45 @@ import FAQ from './Pages/kysymykset';
 import Ostoskori from './Pages/Ostoskori';
 import Lautapelit from './Pages/Lautapelit';
 import {Route, Routes} from 'react-router';
+import {useState, useEffect} from 'react';
 
 const url = 'http://localhost/verkkopalveluprojekti-backend/';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    if ('cart' in localStorage) {
+      setCart(JSON.parse(localStorage.getIitem('cart')));
+    }
+  }, [])
+
+  function addToCart(product) {
+    if (cart.some(item => item.id === product.id)) {
+      const existingProduct = cart.filter(item => item.id === product.id);
+      updateAmount(parseInt(existingProduct[0].amount) + 1,product);
+    } else {
+      product['amount'] = 1;
+      const newCart = [...cart,product];
+      setCart(newCart);
+      localStorage.setItem('cart',JSON.stringify(newCart));
+
+    }
+  }
+
+  function removeFromCart(product) {
+    const itemsWithoutRemoved = cart.filter(item = item.id !== product.id);
+    setCart(itemsWithoutRemoved);
+    localStorage.setItem('cart',JSON.stringify(itemsWithoutRemoved));
+  }
+
+  function updateAmount(amount, product) {
+    product.amount = amount;
+    const index = cart.findIndex((item => item.id === product.id));
+    const modifiedCart = Object.assign([...cart],{[index]: product});
+    setCart(modifiedCart);
+    localStorage.setItem('cart',JSON.stringify(modifiedCart));
+  }
   
   return (
     <div className="App" >
