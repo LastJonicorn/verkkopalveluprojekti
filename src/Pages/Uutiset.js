5b2e64c4ff63902644cd490cbe01374c476ed6b4
liisapikkarainen <liisapikkarainen1@gmail.com>
2022-04-12 11:27:59 +0300

    Accordion uutiset-sivulle ja sinne vähän sisältöä

--- src/Pages/Uutiset.js
+++ src/Pages/Uutiset.js
@@ -1,47 +1,26 @@
 import React from 'react';
 import '../App.css';
+import '../Uutiset.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Carousel from 'react-bootstrap/Carousel';
+import Accordion from 'react-bootstrap/Accordion';
 
 function App() {
   return (    
-    <Carousel fade>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=First slide&bg=373940"
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Second slide&bg=282c34"
-        alt="Second slide"
-      />
-  
-      <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Third slide&bg=20232a"
-        alt="Third slide"
-      />
-  
-      <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
+    <Accordion className='accordion'>
+  <Accordion.Item eventKey="0">
+    <Accordion.Header>Pääsiäistarjoukset!</Accordion.Header>
+    <Accordion.Body>
+      Kaikki kortti- ja strategiapelit -20% 8. - 17.4. välisen ajan!
+    </Accordion.Body>
+  </Accordion.Item>
+  <Accordion.Item eventKey="1">
+    <Accordion.Header>Olemme lisänneet valikoimaamme runsaasti lautapelejä: katso lista!</Accordion.Header>
+    <Accordion.Body>
+      
+    </Accordion.Body>
+  </Accordion.Item>
+</Accordion>
+
   );
 }
 
