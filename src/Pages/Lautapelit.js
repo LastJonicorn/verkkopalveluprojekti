11673b823fdb8168c7df12d453244a7fdfc720b6
LastJonicorn <joni.k.lampela@gmail.com>
2022-04-13 10:44:33 +0300

    Kuvat näkyy

--- src/Pages/Lautapelit.js
+++ src/Pages/Lautapelit.js
@@ -32,58 +32,16 @@ export default function Products({url}) {
 
     <div className="App">
       <div>
+        <h2>{categoryName}</h2>
       {products.map(tuote => (
         <div key={tuote.tuotenro}>
-          {tuote.tuotenimi}
+          <h3>{tuote.tuotenimi}</h3>
+          <div>
+            <img src={url + '/images/' + tuote.kuva} alt='Loading'/>
+          </div>
         </div>
       ))}
     </div>
-
-
-      <div className="row">
-      <div className="col-7">
-
-      <div className="row row-cols-1 row-cols-md-2 g-4">
-  <div className="col">
-    <div className="card">
-      <img src="../Kuvat/lautapeli_1_monopoly.jpg" className="lautapeli1" alt="..."></img>
-      <div className="card-body">
-        <h5 className="card-title">Monopoly</h5>
-        <p className="card-text">Klassikkojen klassikko! Tätä et ikinä kyllästy pelaamaan.</p>
-      </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card">
-      <img src="../Kuvat/lautapeli_2_kimble.jpg" className="lautapeli2" alt="..."></img>
-      <div className="card-body">
-        <h5 className="card-title">Kimble</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card">
-      <img src="../Kuvat/lautapeli_3_muuttuva_labyrintti.jpg" className="lautapeli3" alt="..."></img>
-      <div className="card-body">
-        <h5 className="card-title">Muuttuva Labyrintti</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card">
-      <img src="../Kuvat/lautapeli_4_el_dorado.jpg" className="lautapeli4" alt="..."></img>
-      <div className="card-body">
-        <h5 className="card-title">El Dorado</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-</div>
-      
-    </div>
-  </div>  
   </div>
   );
   
