f7449f977a68e4e8d0ad72a081aeecece7146c27
BENJAMIN_MADETOJA <benjaminmadetoja@gmail.com>
2022-04-24 19:07:22 +0300

    Palautteen värkkäystä

--- src/Pages/Palaute.js
+++ src/Pages/Palaute.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.css';
